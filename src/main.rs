6803f2eb8d009498696c0a6632d8efa173402dcd
Carlos Alberto Borges Garcia Jr <carlosgarcia@pucminas.br>
2023-10-05 08:16:18 -0300

    Added commandline switches
    To run the demo program, use: --demo. To run a rom file, use --rom filename.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 use std::env;
-
+use clap::{Parser, Arg};
 
 use chip8::memory::MAX_MEM;
 use chip8::chip8::PROGRAM_ADDRESS;
@@ -13,108 +13,42 @@ mod chip8;
 fn main() -> Result<(), String> {
     let mut chip8 = CHIP8::new();
     
-    let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
+
+    let mut program = [0; MAX_MEM - PROGRAM_ADDRESS];
+
+    match args.rom.as_str() {
+
+        "" => match args.demo {
+            true => program = get_demo_program(),
+            false => {}
+        }
+        
+        _ => program = read_from_disk(&args.rom)
+    }
     
-    if args.len() < 2 {
-
-    // Play sound for 255 cycles
-    chip8.ram[0x0200] = 0x6E; 
-    chip8.ram[0x0201] = 0xFF;
-    chip8.ram[0x0202] = 0xFE;
-    chip8.ram[0x0203] = 0x18;
-
-    // Clear screen
-    chip8.ram[0x0204] = 0x00;
-    chip8.ram[0x0205] = 0xe0;
-            
-
-        // MOVE RIGHT IF KEY_9 IS PRESSED
-        // Set the value of V0 to 9
-        chip8.ram[0x206] = 0x60;
-        chip8.ram[0x207] = 0x09;
-        // Skip next instruction if key_9 (value in v0) is not pressed
-        chip8.ram[0x208] = 0xE0;
-        chip8.ram[0x209] = 0xA1;
-        // Increments A by 1
-        chip8.ram[0x020A] = 0x7A;
-        chip8.ram[0x020B] = 0x01;
-        // END MOVE RIGHT
+    
+    chip8.load_program(program, program.len());
+    
+    chip8.run()
+}
 
 
-        // MOVE LEFT IF KEY_7 IS PRESSED
-        // Set the value of V1 to 7
-        chip8.ram[0x20C] = 0x61;
-        chip8.ram[0x20D] = 0x07;
-        // Set the value of V4 to 1
-        chip8.ram[0x20E] = 0x64;
-        chip8.ram[0x20F] = 0x01;
-        // Skip next instruction if key_7 (value in v1) is not pressed
-        chip8.ram[0x210] = 0xE1;
-        chip8.ram[0x211] = 0xA1;
-        // Subtract VA by V4
-        chip8.ram[0x0212] = 0x8A;
-        chip8.ram[0x0213] = 0x45;
-        // END MOVE LEFT
 
 
-        // MOVE DOWN IF KEY_0 IS PRESSED
-        // Set the value of V2 to 0
-        chip8.ram[0x214] = 0x62;
-        chip8.ram[0x215] = 0x00;
-        // Skip next instruction if key_0 (value in v2) is not pressed
-        chip8.ram[0x216] = 0xE2;
-        chip8.ram[0x217] = 0xA1;
-        // Increments B by 1
-        chip8.ram[0x0218] = 0x7B;
-        chip8.ram[0x0219] = 0x01;
-        // END MOVE DOWN
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, default_value_t = String::from(""))]
+    rom: String,
 
-        // MOVE UP IF KEY_5 IS PRESSED
-        // Set the value of V3 to 5
-        chip8.ram[0x21A] = 0x63;
-        chip8.ram[0x21B] = 0x05;
-        // Set the value of V4 to 1
-        chip8.ram[0x21C] = 0x64;
-        chip8.ram[0x21D] = 0x01;
-        // Skip next instruction if key_5 (value in v3) is not pressed
-        chip8.ram[0x21E] = 0xE3;
-        chip8.ram[0x21F] = 0xA1;
-        // Subtract VB by V4
-        chip8.ram[0x0220] = 0x8B;
-        chip8.ram[0x0221] = 0x45;
-        // END MOVE LEFT
+    #[arg(short, long, default_value_t = false)]
+    demo: bool
+}
 
-
-        // Set strite in memory
-        chip8.ram[0x022A] = 0xBA;
-        chip8.ram[0x022B] = 0x7C;
-        chip8.ram[0x022C] = 0xD6;
-        chip8.ram[0x022D] = 0xFE;
-        chip8.ram[0x022E] = 0x54;
-        chip8.ram[0x022F] = 0xAA;
-        
-        // Set register i to the address of sprite
-        chip8.ram[0x0222] = 0xA2;
-        chip8.ram[0x0223] = 0x2A;
-
-        // Draw 1 pixel (value in n) tall sprite in the coords A, B (values in VA, VB)
-        chip8.ram[0x0224] = 0xDA;
-        chip8.ram[0x0225] = 0xB6;
-
-        // Wait until a key is pressed
-        chip8.ram[0x0226] = 0xF8;
-        chip8.ram[0x0227] = 0x0A;
-
-        // Infinite loop            
-            // Jump to 0x02A0
-            chip8.ram[0x0228] = 0x12;
-            chip8.ram[0x0229] = 0x04;
-            
-        
-    } else {
-        let mut program = [0; MAX_MEM - PROGRAM_ADDRESS];
-
-        let filename = &args[1];
+fn read_from_disk(filename: &str) -> [u8; MAX_MEM - PROGRAM_ADDRESS] {
+    
+    let mut program = [0; MAX_MEM - PROGRAM_ADDRESS];
 
         let mut i = 0usize;
         let my_buf = BufReader::new(File::open(filename).unwrap());
@@ -124,8 +58,105 @@ fn main() -> Result<(), String> {
             i+=1;
         }
 
-        chip8.load_program(program, i);
-    }
+        program
+}
 
-    chip8.run()
+fn get_demo_program() -> [u8; MAX_MEM - PROGRAM_ADDRESS] {
+
+    let mut program = [0u8; MAX_MEM - PROGRAM_ADDRESS];
+
+    // Play sound for 255 cycles
+    program[0x0000] = 0x6E; 
+    program[0x0001] = 0xFF;
+    program[0x0002] = 0xFE;
+    program[0x0003] = 0x18;
+
+    // Clear screen
+    program[0x0004] = 0x00;
+    program[0x0005] = 0xe0;
+            
+
+        // MOVE RIGHT IF KEY_9 IS PRESSED
+        // Set the value of V0 to 9
+        program[0x006] = 0x60;
+        program[0x007] = 0x09;
+        // Skip next instruction if key_9 (value in v0) is not pressed
+        program[0x008] = 0xE0;
+        program[0x009] = 0xA1;
+        // Increment VA by 1
+        program[0x000A] = 0x7A;
+        program[0x000B] = 0x01;
+        // END MOVE RIGHT
+
+
+        // MOVE LEFT IF KEY_7 IS PRESSED
+        // Set the value of V1 to 7
+        program[0x00C] = 0x61;
+        program[0x00D] = 0x07;
+        // Set the value of V4 to 1
+        program[0x00E] = 0x64;
+        program[0x00F] = 0x01;
+        // Skip next instruction if key_7 (value in v1) is not pressed
+        program[0x010] = 0xE1;
+        program[0x011] = 0xA1;
+        // Subtract VA by V4
+        program[0x0012] = 0x8A;
+        program[0x0013] = 0x45;
+        // END MOVE LEFT
+
+
+        // MOVE DOWN IF KEY_0 IS PRESSED
+        // Set the value of V2 to 0
+        program[0x014] = 0x62;
+        program[0x015] = 0x00;
+        // Skip next instruction if key_0 (value in v2) is not pressed
+        program[0x016] = 0xE2;
+        program[0x017] = 0xA1;
+        // Increments B by 1
+        program[0x0018] = 0x7B;
+        program[0x0019] = 0x01;
+        // END MOVE DOWN
+
+        // MOVE UP IF KEY_5 IS PRESSED
+        // Set the value of V3 to 5
+        program[0x01A] = 0x63;
+        program[0x01B] = 0x05;
+        // Set the value of V4 to 1
+        program[0x01C] = 0x64;
+        program[0x01D] = 0x01;
+        // Skip next instruction if key_5 (value in v3) is not pressed
+        program[0x01E] = 0xE3;
+        program[0x01F] = 0xA1;
+        // Subtract VB by V4
+        program[0x0020] = 0x8B;
+        program[0x0021] = 0x45;
+        // END MOVE LEFT
+
+
+        // Set strite in memory
+        program[0x002A] = 0xBA;
+        program[0x002B] = 0x7C;
+        program[0x002C] = 0xD6;
+        program[0x002D] = 0xFE;
+        program[0x002E] = 0x54;
+        program[0x002F] = 0xAA;
+        
+        // Set register i to the address of sprite
+        program[0x0022] = 0xA2;
+        program[0x0023] = 0x2A;
+
+        // Draw 1 pixel (value in n) tall sprite in the coords A, B (values in VA, VB)
+        program[0x0024] = 0xDA;
+        program[0x0025] = 0xB6;
+
+        // Wait until a key is pressed
+        program[0x0026] = 0xF8;
+        program[0x0027] = 0x0A;
+
+        // Infinite loop            
+            // Jump to 0x02A0
+            program[0x0028] = 0x12;
+            program[0x0029] = 0x04;
+        
+    program
 }
